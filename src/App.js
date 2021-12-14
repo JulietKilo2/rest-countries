5f8ff0138e93b628c498f8f67bc18bdab59650ee
Jisub <jisubkim02@gmail.com>
2021-12-14 15:16:48 +0900

    added header and search bar ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function App() {
-  return <div>Rest Countries API Project</div>;
+  return (
+    <div className="app">
+      <header>
+        <h1>Where in the world?</h1>
+        <div className="darkmode-container">
+          <span>
+            <i className="far fa-moon"></i>
+          </span>
+          <span>Dark Mode</span>
+        </div>
+      </header>
+      <section>
+        <div className="search-input">
+          <i className="fas fa-search"></i>
+          <input type="text" placeholder="Search for a country..." />
+        </div>
+        <div className="filter-input">
+          <select name="" id="">
+            <option value="">Filter by Region</option>
+            <option value="">Africa</option>
+            <option value="">America</option>
+            <option value="">Asia</option>
+            <option value="">Europe</option>
+            <option value="">Oceania</option>
+          </select>
+        </div>
+      </section>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
